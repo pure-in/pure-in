46fa3176d3a63fc5f60dd8fea7f304a24f7fe333
Zul Faza Makarima <zulfazamakarima@gmail.com>
2022-07-04 10:48:37 +0700

    update kualitas udara slice

--- src/slices/KualitasUdara/KualitasUdara.tsx
+++ src/slices/KualitasUdara/KualitasUdara.tsx
@@ -1,15 +1,11 @@
 import { SliceComponentProps } from '@prismicio/react';
 import { openWeather } from 'core/axios';
-import { subDays, format } from 'date-fns';
+import { subDays } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import { KualitasUdaraType } from '.';
-import Fair from './JenisKualitasUdara/Fair';
-import Good from './JenisKualitasUdara/Good';
-import Moderate from './JenisKualitasUdara/Moderate';
-import Poor from './JenisKualitasUdara/Poor';
-import VeryPoor from './JenisKualitasUdara/VeryPoor';
+import AirPollutionItem from './AirPollutionItem';
 
-type AirPollutionType = {
+export type AirPollutionType = {
 	dt: number;
 	main: {
 		aqi: number;
@@ -43,7 +39,7 @@ const KualitasUdara = ({ slice }: SliceComponentProps<KualitasUdaraType>) => {
 				},
 			})
 			.then((res) => {
-				setAirPollutions(res.data.list);
+				setAirPollutions(res.data.list.reverse());
 			});
 	}, []);
 
@@ -58,43 +54,7 @@ const KualitasUdara = ({ slice }: SliceComponentProps<KualitasUdaraType>) => {
 					className="flex items-center flex-col gap-5 w-full max-h-96 overflow-y-auto"
 				>
 					{AirPollutions.map((air) => (
-						<div
-							className=" w-max gap-10 rounded border flex-bc px-10 py-5"
-							key={air.dt}
-						>
-							<div>{format(air.dt * 1000, 'do LLL yyyy HH:mm a')}</div>
-							<div className="flex gap-4">
-								<div className="flex-cc flex-col">
-									{
-										{
-											'0': <Good />,
-											'1': <Fair />,
-											'2': <Moderate />,
-											'3': <Poor />,
-											'4': <VeryPoor />,
-										}[air.main.aqi - 1]
-									}
-								</div>
-								<div className="w-24 h-16 bg-gray-300 rounded-md flex-cc flex-col">
-									<span className="text-gray-500 text-sm">NO2</span>
-									<span className="text-gray-500 text-md">
-										{air.components.no2}
-									</span>
-								</div>
-								<div className="w-24 h-16 bg-gray-300 rounded-md flex-cc flex-col">
-									<span className="text-gray-500 text-sm">PM10</span>
-									<span className="text-gray-500 text-md">
-										{air.components.pm10}
-									</span>
-								</div>
-								<div className="w-24 h-16 bg-gray-300 rounded-md flex-cc flex-col">
-									<span className="text-gray-500 text-sm">CO</span>
-									<span className="text-gray-500 text-md">
-										{air.components.co}
-									</span>
-								</div>
-							</div>
-						</div>
+						<AirPollutionItem key={air.dt} air={air} />
 					))}
 				</div>
 			</div>
